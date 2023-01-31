2dbd60bbcb03aa03aee09880937ef2372751f75c
Mike Perham <mperham@gmail.com>
2023-01-30 17:52:31 -0800

    first commit

---
+++ lib/deprecatedrb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "deprecatedrb/version"
+
+module Deprecatedrb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_deprecatedrb.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestDeprecatedrb < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Deprecatedrb::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
